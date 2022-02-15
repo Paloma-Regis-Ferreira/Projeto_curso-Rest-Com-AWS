de2c0c5a418e0b1f58d7ad7f26d933e37c34559a
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-02-14 21:47:51 -0300

    implementando bco de dados

---
+++ teste/src/teste/teste.java
@@ -0,0 +1,17 @@
+package teste;
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		for(int i = 0 ; i <= 10; i++){
+			
+			if(i == 5){
+				i +=4;
+		}
+			System.out.println(i);
+	}
+
+}
+}
