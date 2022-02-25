affa55994a77a3d8b2d9d3702ea287a94f997cc0
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-02-24 21:20:44 -0300

    customizacao do json

---
+++ rest-with-spring-boot02-customizacaoJson/src/main/java/br/com/paloma/services/PersonService.java
@@ -0,0 +1,54 @@
+package br.com.paloma.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.paloma.converter.DozerConverter;
+import br.com.paloma.data.model.Person;
+import br.com.paloma.data.vo.PersonVO;
+import br.com.paloma.exception.ResourceNotFoundException;
+import br.com.paloma.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO findById(Long id) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+
+	public List<PersonVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+		repository.delete(entity);
+	}
+	
+}
